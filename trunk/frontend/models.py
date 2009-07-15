6c67ce3bc97a074c9223209b42d863c2932b54ee
Alec.Muffett <Alec.Muffett@00126098-1528-11de-8b5c-13accb87f508>
2009-07-15 20:18:18 +0000

    rejig  model to use many2many relations for tags and interests, as well as for: and not:

--- trunk/frontend/models.py
+++ trunk/frontend/models.py
@@ -4,48 +4,45 @@ from django.db import models
 
 MINE_STRING=1024
 
-class Item(models.Model):
-    ITEM_STATUSES=(
-        ( 'PRV', 'Private' ),
-        ( 'SHD', 'Shared' ),
-        ( 'PUB', 'Public' ),
-        ( 'ARQ', 'Authentication Required' ),
-        )
-
-    name = models.CharField(max_length=MINE_STRING)
-    description = models.TextField(blank=True)
-    tags = models.CharField(max_length=MINE_STRING, blank=True)
-    status = models.CharField(max_length=3, choices=ITEM_STATUSES)
-
-    content_type = models.CharField(max_length=MINE_STRING)
-    hide_after = models.DateTimeField(blank=True)
-    hide_before = models.DateTimeField(blank=True)
-
+class Tag(models.Model):
+    name = models.CharField(max_length=MINE_STRING, unique=True)
+    implied = models.CharField(max_length=MINE_STRING, blank=True)
     date_created = models.DateTimeField(auto_now_add=True, editable=False)
     date_last_modified = models.DateTimeField(auto_now=True)
 
 class Relation(models.Model):
     name = models.CharField(max_length=MINE_STRING, unique=True)
     description = models.TextField(blank=True)
-    interests = models.CharField(max_length=MINE_STRING, blank=True)
+    interests = models.ManyToManyField(Tag, related_name='x_interests', blank=True)
+    interests_require = models.ManyToManyField(Tag, related_name='x_interests_require', blank=True)
+    interests_exclude = models.ManyToManyField(Tag, related_name='x_interests_exclude', blank=True)
     version = models.PositiveIntegerField()
-
     embargo_after = models.DateTimeField(blank=True)
     embargo_before = models.DateTimeField(blank=True)
     ip_address_pattern = models.CharField(max_length=MINE_STRING, blank=True)
-
     email_address = models.EmailField(max_length=MINE_STRING, blank=True)
     url_callback = models.URLField(max_length=MINE_STRING, blank=True)
     url_homepage = models.URLField(max_length=MINE_STRING, blank=True)
     url_mugshot = models.URLField(max_length=MINE_STRING, blank=True)
-
     date_created = models.DateTimeField(auto_now_add=True, editable=False)
     date_last_modified = models.DateTimeField(auto_now=True)
 
-class Tag(models.Model):
-    name = models.CharField(max_length=MINE_STRING, unique=True)
-    implied = models.CharField(max_length=MINE_STRING, blank=True)
-
+class Item(models.Model):
+    ITEM_STATUSES=(
+	( 'PRV', 'Private' ),
+	( 'SHD', 'Shared' ),
+	( 'PUB', 'Public' ),
+	( 'ARQ', 'Authentication Required' ),
+	)
+    name = models.CharField(max_length=MINE_STRING)
+    description = models.TextField(blank=True)
+    tags = models.ManyToManyField(Tag, related_name='x_tags', blank=True)
+    tags_for_relation = models.ManyToManyField(Relation, related_name='x_tags_for_relation', blank=True)
+    tags_not_relation = models.ManyToManyField(Relation, related_name='x_tags_not_relation', blank=True)
+    status = models.CharField(max_length=3, choices=ITEM_STATUSES)
+    content_type = models.CharField(max_length=MINE_STRING)
+    hide_after = models.DateTimeField(blank=True)
+    hide_before = models.DateTimeField(blank=True)
     date_created = models.DateTimeField(auto_now_add=True, editable=False)
     date_last_modified = models.DateTimeField(auto_now=True)
 
@@ -53,9 +50,7 @@ class Comment(models.Model):
     title = models.CharField(max_length=MINE_STRING)
     body = models.TextField(blank=True)
     likes_this = models.BooleanField(default=False)
-
     item = models.ForeignKey(Item, editable=False)
     relation = models.ForeignKey(Relation, editable=False)
-
     date_created = models.DateTimeField(auto_now_add=True, editable=False)
     date_last_modified = models.DateTimeField(auto_now=True)
