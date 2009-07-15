74612a55ef619bd9292928e7fd7be85555134c39
Alec.Muffett <Alec.Muffett@00126098-1528-11de-8b5c-13accb87f508>
2009-07-15 22:23:00 +0000

    using admin classes to structure data

--- trunk/frontend/admin.py
+++ trunk/frontend/admin.py
@@ -1,7 +1,31 @@
 from pymine.frontend.models import Tag, Relation, Item, Comment
 from django.contrib import admin
 
-admin.site.register(Tag)
-admin.site.register(Relation)
-admin.site.register(Item)
-admin.site.register(Comment)
+class TagAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['name']}), 
+        ('Implying Tags', {'fields': ['implied'], 'classes': ['collapse']}), 
+        ('Advanced', {'fields': ['implied_cache'], 'classes': ['collapse']}), 
+        ]
+
+class RelationAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['name', 'version', 'description', 'interests']}), 
+        ('Advanced Tags', {'fields': ['interests_exclude', 'interests_require'], 'classes': ['collapse']}), 
+        ('Contact Information', {'fields': ['email_address', 'url_homepage', 'url_image', 'url_callback']}), 
+        ('Access Control', {'fields': ['embargo_before', 'embargo_after', 'ip_address_pattern'], 'classes': ['collapse']}), 
+        ]
+
+class ItemAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ]
+
+class CommentAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ]
+
+
+admin.site.register(Tag, TagAdmin)
+admin.site.register(Relation, RelationAdmin)
+admin.site.register(Item, ItemAdmin)
+admin.site.register(Comment, CommentAdmin)
