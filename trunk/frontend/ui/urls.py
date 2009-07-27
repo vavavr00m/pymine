c9e82cdcbeebc87a3165cb1d49645eedb6bb20e7
Alec.Muffett <Alec.Muffett@00126098-1528-11de-8b5c-13accb87f508>
2009-07-27 20:57:15 +0000

    i hate conflicts

--- trunk/frontend/ui/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls.defaults import *
-
-urlpatterns = patterns('',
-    (r'^weblog/',        include('django_website.apps.blog.urls.blog')),
-    (r'^documentation/', include('django_website.apps.docs.urls.docs')),
-    (r'^comments/',      include('django.contrib.comments.urls')),
-)
