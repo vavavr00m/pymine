37da71d306b714905da1ff28cbfe5178df58bacb
Alec.Muffett <Alec.Muffett@00126098-1528-11de-8b5c-13accb87f508>
2009-07-28 18:46:48 +0000

    code formatting

--- mine/models.py
+++ mine/models.py
@@ -16,22 +16,33 @@
 
 from django.db import models
 
-# Create your models here.
-
 MINE_STRING=1024
 EDIT_BACKDOOR=True
 
+##################################################################
+
 class Tag(models.Model):
-    name = models.CharField(max_length=MINE_STRING, unique=True)
+    name = models.CharField(max_length=MINE_STRING,
+                            unique=True)
+
     description = models.TextField(blank=True)
-    implies = models.ManyToManyField('self', symmetrical=False, related_name='x_implies', null=True, blank=True)
-    cached_implications = models.ManyToManyField('self', 
-                                                 symmetrical=False, 
-                                                 related_name='x_cached_implications', 
-                                                 null=True, 
-                                                 blank=True, 
+
+    implies = models.ManyToManyField('self',
+                                     symmetrical=False,
+                                     related_name='x_implies',
+                                     null=True,
+                                     blank=True)
+
+    cached_implications = models.ManyToManyField('self',
+                                                 symmetrical=False,
+                                                 related_name='x_cached_implications',
+                                                 null=True,
+                                                 blank=True,
                                                  editable=EDIT_BACKDOOR)
-    created = models.DateTimeField(auto_now_add=True, editable=EDIT_BACKDOOR)
+
+    created = models.DateTimeField(auto_now_add=True,
+                                   editable=EDIT_BACKDOOR)
+
     last_modified = models.DateTimeField(auto_now=True)
 
     class Meta: ordering = ['id']
@@ -44,95 +55,185 @@ class Tag(models.Model):
             'tagCreated': self.created.isoformat(),
             'tagLastModified': self.last_modified.isoformat(),
             }
+
     def inbound(self):
         pass
-    def __unicode__(self): 
+
+    def __unicode__(self):
         return self.name
 
 
+##################################################################
+
 class Relation(models.Model):
-    name = models.CharField(max_length=MINE_STRING, unique=True)
+    name = models.CharField(max_length=MINE_STRING,
+                            unique=True)
+
     description = models.TextField(blank=True)
-    tags = models.ManyToManyField(Tag, related_name='relations_with_tag', null=True, blank=True)
-    tags_required = models.ManyToManyField(Tag, related_name='relations_requiring', null=True, blank=True)
-    tags_excluded = models.ManyToManyField(Tag, related_name='relations_excluding', null=True, blank=True)
+
+    tags = models.ManyToManyField(Tag,
+                                  related_name='relations_with_tag',
+                                  null=True,
+                                  blank=True)
+
+    tags_required = models.ManyToManyField(Tag,
+                                           related_name='relations_requiring',
+                                           null=True,
+                                           blank=True)
+
+    tags_excluded = models.ManyToManyField(Tag,
+                                           related_name='relations_excluding',
+                                           null=True,
+                                           blank=True)
+
     version = models.PositiveIntegerField(default=1)
-    embargo_after = models.DateTimeField(null=True, blank=True)
-    embargo_before = models.DateTimeField(null=True, blank=True)
-    network_pattern = models.CharField(max_length=MINE_STRING, blank=True)
-    email_address = models.EmailField(max_length=MINE_STRING, blank=True)
-    url_callback = models.URLField(max_length=MINE_STRING, blank=True)
-    url_homepage = models.URLField(max_length=MINE_STRING, blank=True)
-    url_image = models.URLField(max_length=MINE_STRING, blank=True)
-    created = models.DateTimeField(auto_now_add=True, editable=EDIT_BACKDOOR)
+
+    embargo_after = models.DateTimeField(null=True,
+                                         blank=True)
+
+    embargo_before = models.DateTimeField(null=True,
+                                          blank=True)
+
+    network_pattern = models.CharField(max_length=MINE_STRING,
+                                       blank=True)
+
+    email_address = models.EmailField(max_length=MINE_STRING,
+                                      blank=True)
+
+    url_callback = models.URLField(max_length=MINE_STRING,
+                                   blank=True)
+
+    url_homepage = models.URLField(max_length=MINE_STRING,
+                                   blank=True)
+
+    url_image = models.URLField(max_length=MINE_STRING,
+                                blank=True)
+
+    created = models.DateTimeField(auto_now_add=True,
+                                   editable=EDIT_BACKDOOR)
+
     last_modified = models.DateTimeField(auto_now=True)
 
     class Meta: ordering = ['name']
 
     def outbound(self):
         return { }
+
     def inbound(self):
         pass
-    def __unicode__(self): 
+
+    def __unicode__(self):
         return self.name
+
+##################################################################
 
 class Item(models.Model):
     ITEM_STATUSES=(
-	( 'PRV', 'Private' ),
-	( 'SHD', 'Shared' ),
-	( 'PUB', 'Public' ),
-	( 'ARQ', 'Authentication Required' ),
+	( 'X', 'Private' ),
+	( 'S', 'Shared' ),
+	( 'P', 'Public' ),
+	( 'A', 'AuthRequired' ),
 	)
+
     name = models.CharField(max_length=MINE_STRING)
+
     description = models.TextField(blank=True)
-    tags = models.ManyToManyField(Tag, related_name='items_tagged', null=True, blank=True)
-    item_for_relations = models.ManyToManyField(Relation, related_name='items_explicitly_for', null=True, blank=True)
-    item_not_relations = models.ManyToManyField(Relation, related_name='items_explicitly_not', null=True, blank=True)
-    status = models.CharField(max_length=3, choices=ITEM_STATUSES)
+
+    tags = models.ManyToManyField(Tag,
+                                  related_name='items_tagged',
+                                  null=True,
+                                  blank=True)
+
+    item_for_relations = models.ManyToManyField(Relation,
+                                                related_name='items_explicitly_for',
+                                                null=True,
+                                                blank=True)
+
+    item_not_relations = models.ManyToManyField(Relation,
+                                                related_name='items_explicitly_not',
+                                                null=True,
+                                                blank=True)
+
+    status = models.CharField(max_length=1,
+                              choices=ITEM_STATUSES)
+
     content_type = models.CharField(max_length=MINE_STRING)
-    hide_after = models.DateTimeField(null=True, blank=True)
-    hide_before = models.DateTimeField(null=True, blank=True)
-    created = models.DateTimeField(auto_now_add=True, editable=EDIT_BACKDOOR)
+
+    hide_after = models.DateTimeField(null=True,
+                                      blank=True)
+
+    hide_before = models.DateTimeField(null=True,
+                                       blank=True)
+
+    created = models.DateTimeField(auto_now_add=True,
+                                   editable=EDIT_BACKDOOR)
+
     last_modified = models.DateTimeField(auto_now=True)
 
     class Meta: ordering = ['-last_modified']
 
     def outbound(self):
         return { }
+
     def inbound(self):
         pass
-    def __unicode__(self): 
+
+    def __unicode__(self):
         return self.name
+
+##################################################################
 
 class Comment(models.Model):
     title = models.CharField(max_length=MINE_STRING)
+
     body = models.TextField(blank=True)
+
     likes = models.BooleanField(default=False)
-    item = models.ForeignKey(Item, editable=EDIT_BACKDOOR)
-    relation = models.ForeignKey(Relation, editable=EDIT_BACKDOOR)
-    created = models.DateTimeField(auto_now_add=True, editable=EDIT_BACKDOOR)
+
+    item = models.ForeignKey(Item,
+                             editable=EDIT_BACKDOOR)
+
+    relation = models.ForeignKey(Relation,
+                                 editable=EDIT_BACKDOOR)
+
+    created = models.DateTimeField(auto_now_add=True,
+                                   editable=EDIT_BACKDOOR)
+
     last_modified = models.DateTimeField(auto_now=True)
 
     class Meta: ordering = ['-id']
 
     def outbound(self):
         return { }
+
     def inbound(self):
         pass
-    def __unicode__(self): 
+
+    def __unicode__(self):
         return self.title
 
+##################################################################
+
 class VanityURL(models.Model):
-    name = models.CharField(max_length=MINE_STRING, unique=True)
+    name = models.CharField(max_length=MINE_STRING,
+                            unique=True)
+
     link = models.TextField(blank=True)
-    created = models.DateTimeField(auto_now_add=True, editable=EDIT_BACKDOOR)
+
+    created = models.DateTimeField(auto_now_add=True,
+                                   editable=EDIT_BACKDOOR)
+
     last_modified = models.DateTimeField(auto_now=True)
 
     class Meta: ordering = ['-id']
 
     def outbound(self):
         return { }
+
     def inbound(self):
         pass
-    def __unicode__(self): 
+
+    def __unicode__(self):
         return self.name
+
+##################################################################
