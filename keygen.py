da75c4982ba4b85ee709eae5d56adc0999d16136
Alec.Muffett <Alec.Muffett@00126098-1528-11de-8b5c-13accb87f508>
2009-08-12 08:42:33 +0000

    keygen stuff

---
+++ keygen.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+
+import sys
+import getopt
+import base64
+
+rngdev = "/dev/urandom"
+
+def usage():
+    print "wtf?" # let nobody ever say i don't write documentation
+
+def main(argv):
+    nbytes = 66
+
+    try:
+	opts, args = getopt.getopt(argv, "hn:", ["help", "nbytes="])
+
+    except getopt.GetoptError:
+	usage()
+	sys.exit(1)
+
+    for opt, arg in opts:
+	if opt in ('-h', '--help'):
+	    usage()
+	    sys.exit()
+	elif opt in ('-n', '--nbytes'):
+	    nbytes = int(arg)
+
+    rng = open(rngdev, "rb")
+    bytes = rng.read(nbytes)
+    rng.close()
+
+    print base64.b64encode(bytes, '.,')
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
--- runme-setup.sh
+++ runme-setup.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+MINE_USER=$USER
+MINE_DB=database/$MINE_USER/sqlite3.db
+
 YESNO() {
     while :
     do
@@ -14,7 +17,8 @@ YESNO() {
     done
 }
 
-MINE_USER=$USER
+make clean
+make perms
 
 if [ -f settings.py ]
 then
@@ -26,39 +30,48 @@ then
 fi
 
 
-DB=database/$MINE_USER/sqlite3.db
-
-if [ -f $DB ]
+if [ -f $MINE_DB ]
 then
-    echo "The mine database '$DB' already exists"
+    echo "The mine database '$MINE_DB' already exists"
     if YESNO "Shall we nuke it and replace it (THIS WILL IRREVOCABLY DESTROY YOUR MINE)"
     then
-        rm $DB
+        rm $MINE_DB
     fi
 fi
 
-while :
-do
-    echo "Enter your e-mail address: \c"
-    read MINE_EMAIL 
+if  [ "x$MINE_EMAIL" = "x" ] 
+then
+    while :
+    do
+	echo "Enter your e-mail address: \c"
+	read MINE_EMAIL 
+	YESNO "You entered $MINE_EMAIL ; is this OK" && break
+    done
+fi
 
-    YESNO "You entered $MINE_EMAIL ; is this OK" && break
-done
+# yes the args are visible during creation, no i can't be arsed yet to fix this
+MINE_SECRET=`./keygen.py`
 
 if [ ! -f settings.py ]
 then
     cat settings.py.tmpl |
+    sed -e "s/%%MINE_SECRET%%/$MINE_SECRET/g" |
     sed -e "s/%%MINE_USER%%/$MINE_USER/g" |
     sed -e "s/%%MINE_EMAIL%%/$MINE_EMAIL/g" > settings.py 
 fi
 
-test -d database/$MINE_USER || mkdir database/$MINE_USER || exit 1
+if [ ! -d database/$MINE_USER ]
+then
+    mkdir database/$MINE_USER || exit 1
+    chmod 755 database/$MINE_USER
+fi
 
-make clean
-make perms
+# go for it
 make dbsync
 
+# last step
 echo "We shall use '$MINE_USER' as your mine database superuser login name."
 echo "You will now be asked to select a password for '$MINE_USER'"
 python manage.py createsuperuser --username $MINE_USER --email $MINE_EMAIL
 
+exit 0 
