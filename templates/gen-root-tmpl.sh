ac0a2f8c5bb6fd3812890e51169e28ebd3eb0047
Alec.Muffett <Alec.Muffett@00126098-1528-11de-8b5c-13accb87f508>
2009-09-10 14:25:41 +0000

    fixes

--- templates/gen-root-tmpl.sh
+++ templates/gen-root-tmpl.sh
@@ -1,12 +1,11 @@
 for i in api archive doc get mine pub ui
 do
 cat > root-$i.html <<EOF
-{% extends "base-page" %}
+{% extends 'base-page.html' %}
 {% block title %}$i root{% endblock %}
 root-$i.html
 {% block content %}
-{% include 'developer-boilerplate' %}
+{% include 'developer-boilerplate.html' %}
 {% endblock %}
 EOF
 done
-
