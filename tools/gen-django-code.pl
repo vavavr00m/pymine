b0f01c067d4fc9f46dd55d92c3b45cefbb4bea54
Alec.Muffett <Alec.Muffett@00126098-1528-11de-8b5c-13accb87f508>
2009-10-07 11:05:15 +0000

    ...

--- tools/gen-django-code.pl
+++ tools/gen-django-code.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl
 
 $indent = "    ";
-$root_path_strip = "/(api|ui|ie|get|r|v)\b";
+$root_path_strip = "/(api|ui|ie|get|r|v)";
 
 # $value text in <anglebrackets> must be same as lcase($key)
 %regexp_lookup = (
@@ -21,6 +21,8 @@ $root_path_strip = "/(api|ui|ie|get|r|v)\b";
 		   'RATTR',    '?P<rattr>[A-Za-z][_A-Za-z]*',       #  f,  foo,  fooBar
 
 		   'MINEKEY',  '?P<minekey>[A-Za-z0-9!@]+',
+
+		   'EFMT',     '?P<efmt>(zip|tar)',
 
 		   'SUFFIX',   '?P<suffix>.+',
     );
@@ -125,92 +127,90 @@ __END__;
 mine   GET    /                              root-mine
 mine   GET    /pub                           root-pub
 
-api    GET    /api                           root-api
-api    POST   /api/clone/IID.FMT             create-clone
+api    GET    /api                           root-api 
+api    POST   /api/clone/IID.FMT             create-clone 
 api    GET    /api/clone/IID.FMT             list-clones # IID==0 -> list all clones
-api    DELETE /api/comment/CID.FMT           delete-comment
-api    GET    /api/comment/CID.FMT           read-comment
-api    POST   /api/comment/CID.FMT           update-comment
-api    DELETE /api/comment/CID/SATTR.SAFMT   delete-comment-key
-api    GET    /api/comment/CID/SATTR.SAFMT   get-comment-key
+api    DELETE /api/comment/CID.FMT           delete-comment 
+api    GET    /api/comment/CID.FMT           read-comment 
+api    POST   /api/comment/CID.FMT           update-comment 
+api    DELETE /api/comment/CID/SATTR.SAFMT   delete-comment-key 
+api    GET    /api/comment/CID/SATTR.SAFMT   get-comment-key 
 api    POST   /api/comment/item/IID.FMT      create-comment # IID==0 -> comment on entire feed
 api    GET    /api/comment/item/IID.FMT      list-comments # IID==0 -> list all comments
-api    POST   /api/item.FMT                  create-item
-api    GET    /api/item.FMT                  list-items
+api    GET    /api/ie/export.EFMT            export-mine 
+api    GET    /api/ie/import.EFMT            import-mine 
+api    POST   /api/item.FMT                  create-item 
+api    GET    /api/item.FMT                  list-items 
 api    GET    /api/item/IID                  read-item-data # this is the raw item method
-api    DELETE /api/item/IID.FMT              delete-item
-api    GET    /api/item/IID.FMT              read-item
-api    POST   /api/item/IID.FMT              update-item
-api    DELETE /api/item/IID/SATTR.SAFMT      delete-item-key
-api    GET    /api/item/IID/SATTR.SAFMT      get-item-key
-api    GET    /api/registry.FMT              list-registry
-api    POST   /api/registry/RATTR.FMT        amend-registry-key
-api    DELETE /api/registry/RATTR.FMT        delete-registry-key
-api    GET    /api/registry/RATTR.FMT        get-registry-key
-api    POST   /api/relation.FMT              create-relation
-api    GET    /api/relation.FMT              list-relations
-api    DELETE /api/relation/RID.FMT          delete-relation
-api    GET    /api/relation/RID.FMT          read-relation
-api    POST   /api/relation/RID.FMT          update-relation
-api    DELETE /api/relation/RID/SATTR.SAFMT  delete-relation-key
-api    GET    /api/relation/RID/SATTR.SAFMT  get-relation-key
-api    GET    /api/select/item.FMT           read-select-item
-api    GET    /api/select/relation.FMT       read-select-relation
-api    GET    /api/select/tag.FMT            read-select-tag
-api    GET    /api/select/vurl.FMT           read-select-tag
-api    POST   /api/tag.FMT                   create-tag
-api    GET    /api/tag.FMT                   list-tags
-api    DELETE /api/tag/TID.FMT               delete-tag
-api    GET    /api/tag/TID.FMT               read-tag
-api    POST   /api/tag/TID.FMT               update-tag
-api    DELETE /api/tag/TID/SATTR.SAFMT       delete-tag-key
-api    GET    /api/tag/TID/SATTR.SAFMT       get-tag-key
-api    GET    /api/url/RID.FMT               encode-minekey1
-api    GET    /api/url/RID/IID.FMT           encode-minekey2
-api    GET    /api/url/RID/RVSN/IID.FMT      encode-minekey3
-api    GET    /api/version.FMT               read-version
-api    POST   /api/vurl.FMT                  create-vurl
-api    GET    /api/vurl.FMT                  list-vurls
-api    DELETE /api/vurl/TID.FMT              delete-vurl
-api    GET    /api/vurl/TID.FMT              read-vurl
-api    POST   /api/vurl/TID.FMT              update-vurl
-api    DELETE /api/vurl/TID/SATTR.SAFMT      delete-vurl-key
-api    GET    /api/vurl/TID/SATTR.SAFMT      get-vurl-key
+api    DELETE /api/item/IID.FMT              delete-item 
+api    GET    /api/item/IID.FMT              read-item 
+api    POST   /api/item/IID.FMT              update-item 
+api    DELETE /api/item/IID/SATTR.SAFMT      delete-item-key 
+api    GET    /api/item/IID/SATTR.SAFMT      get-item-key 
+api    GET    /api/registry.FMT              list-registry 
+api    POST   /api/registry/RATTR.FMT        amend-registry-key 
+api    DELETE /api/registry/RATTR.FMT        delete-registry-key 
+api    GET    /api/registry/RATTR.FMT        get-registry-key 
+api    POST   /api/relation.FMT              create-relation 
+api    GET    /api/relation.FMT              list-relations 
+api    DELETE /api/relation/RID.FMT          delete-relation 
+api    GET    /api/relation/RID.FMT          read-relation 
+api    POST   /api/relation/RID.FMT          update-relation 
+api    DELETE /api/relation/RID/SATTR.SAFMT  delete-relation-key 
+api    GET    /api/relation/RID/SATTR.SAFMT  get-relation-key 
+api    GET    /api/select/item.FMT           read-select-item 
+api    GET    /api/select/relation.FMT       read-select-relation 
+api    GET    /api/select/tag.FMT            read-select-tag 
+api    GET    /api/select/vurl.FMT           read-select-tag 
+api    POST   /api/tag.FMT                   create-tag 
+api    GET    /api/tag.FMT                   list-tags 
+api    DELETE /api/tag/TID.FMT               delete-tag 
+api    GET    /api/tag/TID.FMT               read-tag 
+api    POST   /api/tag/TID.FMT               update-tag 
+api    DELETE /api/tag/TID/SATTR.SAFMT       delete-tag-key 
+api    GET    /api/tag/TID/SATTR.SAFMT       get-tag-key 
+api    GET    /api/url/RID.FMT               encode-minekey1 
+api    GET    /api/url/RID/IID.FMT           encode-minekey2 
+api    GET    /api/url/RID/RVSN/IID.FMT      encode-minekey3 
+api    GET    /api/version.FMT               read-version 
+api    POST   /api/vurl.FMT                  create-vurl 
+api    GET    /api/vurl.FMT                  list-vurls 
+api    DELETE /api/vurl/TID.FMT              delete-vurl 
+api    GET    /api/vurl/TID.FMT              read-vurl 
+api    POST   /api/vurl/TID.FMT              update-vurl 
+api    DELETE /api/vurl/TID/SATTR.SAFMT      delete-vurl-key 
+api    GET    /api/vurl/TID/SATTR.SAFMT      get-vurl-key 
 
-get    GET    /get                           root-get
-get    GET    /get/MINEKEY                   read-minekey
-get    POST   /get/MINEKEY                   submit-minekey
-get    GET    /r/VURLKEY                     redirect-vurlkey # short to long remapping
-get    GET    /v/SUFFIX                      redirect-vurlname # long to long remapping
+get    GET    /get                           root-get 
+get    GET    /get/MINEKEY                   read-minekey 
+get    POST   /get/MINEKEY                   submit-minekey 
+get    GET    /get/r/VURLKEY                 redirect-vurlkey # short to long remapping
+get    GET    /get/v/SUFFIX                  redirect-vurlname # long to long remapping
 
-ie     GET    /ie                            root-ie
-ie     GET    /ie/export                     ie-export
-ie     GET    /ie/import                     ie-import
-
-ui     GET    /ui                            root-ui
-ui     GET    /ui/create-comment/IID.html    create-comment
-ui     GET    /ui/create-item.html           create-item
-ui     GET    /ui/create-relation.html       create-relation
-ui     GET    /ui/create-tag.html            create-tag
-ui     GET    /ui/create-vurl.html           create-vurl
-ui     GET    /ui/delete-comment/CID.html    delete-comment
-ui     GET    /ui/delete-item/IID.html       delete-item
-ui     GET    /ui/delete-relation/RID.html   delete-relation
-ui     GET    /ui/delete-tag/TID.html        delete-tag
-ui     GET    /ui/delete-vurl/VID.html       delete-vurl
-ui     GET    /ui/list-comments/IID.html     list-comments
-ui     GET    /ui/list-items.html            list-items
-ui     GET    /ui/list-relations.html        list-relations
-ui     GET    /ui/list-tags.html             list-tags
-ui     GET    /ui/list-vurls.html            list-vurls
-ui     GET    /ui/read-comment/CID.html      read-comment
-ui     GET    /ui/read-item/IID.html         read-item
-ui     GET    /ui/read-relation/RID.html     read-relation
-ui     GET    /ui/read-tag/TID.html          read-tag
-ui     GET    /ui/read-vurl/VID.html         read-vurl
-ui     GET    /ui/update-comment/CID.html    update-comment
-ui     GET    /ui/update-item/IID.html       update-item
-ui     GET    /ui/update-relation/RID.html   update-relation
-ui     GET    /ui/update-tag/TID.html        update-tag
-ui     GET    /ui/update-vurl/VID.html       update-vurl
-ui     GET    /ui/version.html               read-version
+ui     GET    /ui                            root-ui 
+ui     GET    /ui/create-comment/IID.html    create-comment 
+ui     GET    /ui/create-item.html           create-item 
+ui     GET    /ui/create-relation.html       create-relation 
+ui     GET    /ui/create-tag.html            create-tag 
+ui     GET    /ui/create-vurl.html           create-vurl 
+ui     GET    /ui/delete-comment/CID.html    delete-comment 
+ui     GET    /ui/delete-item/IID.html       delete-item 
+ui     GET    /ui/delete-relation/RID.html   delete-relation 
+ui     GET    /ui/delete-tag/TID.html        delete-tag 
+ui     GET    /ui/delete-vurl/VID.html       delete-vurl 
+ui     GET    /ui/list-comments/IID.html     list-comments 
+ui     GET    /ui/list-items.html            list-items 
+ui     GET    /ui/list-relations.html        list-relations 
+ui     GET    /ui/list-tags.html             list-tags 
+ui     GET    /ui/list-vurls.html            list-vurls 
+ui     GET    /ui/read-comment/CID.html      read-comment 
+ui     GET    /ui/read-item/IID.html         read-item 
+ui     GET    /ui/read-relation/RID.html     read-relation 
+ui     GET    /ui/read-tag/TID.html          read-tag 
+ui     GET    /ui/read-vurl/VID.html         read-vurl 
+ui     GET    /ui/update-comment/CID.html    update-comment 
+ui     GET    /ui/update-item/IID.html       update-item 
+ui     GET    /ui/update-relation/RID.html   update-relation 
+ui     GET    /ui/update-tag/TID.html        update-tag 
+ui     GET    /ui/update-vurl/VID.html       update-vurl 
+ui     GET    /ui/version.html               read-version 
