c2c45850e72864e9167bdc57d1a1fa1a70cc605a
Alec.Muffett <Alec.Muffett@00126098-1528-11de-8b5c-13accb87f508>
2009-10-15 07:23:08 +0000

    remove static directory
    cleanup the name of djwrap so its purpose is more evident

--- django-env.sh
+++ django-env.sh
@@ -1,12 +1,9 @@
 #!/bin/sh
-
+# horrible kludge but it saves typing
 parent=`(cd .. ; pwd)`
-
 PYTHONPATH=$PYTHONPATH:$parent
 export PYTHONPATH
-
 DJANGO_SETTINGS_MODULE=pymine.settings
 export DJANGO_SETTINGS_MODULE
-
 exec "$@"
 exit 1
