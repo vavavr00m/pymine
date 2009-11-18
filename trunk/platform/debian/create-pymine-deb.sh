ccf73160a25f8facbbd9b2952ffc284717136470
Alec.Muffett <Alec.Muffett@00126098-1528-11de-8b5c-13accb87f508>
2009-11-18 15:54:50 +0000

    WSGI integration in progress, under Ubuntu

--- trunk/platform/debian/create-pymine-deb.sh
+++ trunk/platform/debian/create-pymine-deb.sh
@@ -17,8 +17,11 @@
 ##
 
 PYMINE_MAINTAINER='Alec Muffett <alec.muffett@gmail.com>'
+PYPARENT=var/mine
 WORKDIR=/tmp/pydeb$$
-PYPARENT=var
+USERDIR=`pwd`
+
+##################################################################
 
 test -d $WORKDIR || mkdir -p $WORKDIR || exit 1
 
@@ -28,12 +31,13 @@ mkdir -p debian debian/$PYPARENT debian/DEBIAN debian/usr/share/doc/pymine
 
 chmod -R 755 debian
 
+##################################################################
+
 (
     cd debian/$PYPARENT
     svn checkout http://pymine.googlecode.com/svn/trunk/ pymine
     cd pymine
     make perms
-    chown -R www-data database
 )
 
 PYMINE_VERSION=`cd debian/$PYPARENT/pymine ; svn info | awk '/^Revision:/ {print $2}'`
@@ -49,9 +53,9 @@ Architecture: all
 Depends: curl, libapache2-mod-wsgi, make, python, python-beautifulsoup, python-crypto, python-django
 Maintainer: $PYMINE_MAINTAINER
 Description: The Mine Project: Pymine Development Version
- Pymine is a Django/Python tool for implementing a Mine! - software
- that enables personal data sharing via feeds and URLs which are
- different for each subscriber (unlike a traditional blog)
+ Pymine is a Django/Python tool for implementing a Mine! -
+ software which enables personal data sharing via feeds and URLs
+ which are different for each subscriber (unlike a traditional blog)
 EOF
 
 ##################################################################
@@ -113,12 +117,27 @@ chmod -R 755 `find debian/DEBIAN -type f`
 chmod -R 644 `find debian/usr/share/doc/pymine -type f`
 gzip --best debian/usr/share/doc/pymine/changelog*
 
-( cd debian/$PYPARENT/pymine ; rm -r `find . -name .svn` )
+##################################################################
 
-dpkg-deb --build debian || exit 1
+(
+    cd debian/$PYPARENT/pymine
+    rm -rf `find . -name .svn`
+)
+
+##################################################################
+
+perl -pi~ -e 's/^DEBUG\s*=\s*True/DEBUG = False/o' debian/$PYPARENT/pymine/settings.py.tmpl
+
+##################################################################
+
+sudo chown -R root:root debian
+sudo chown -R www-data debian/$PYPARENT/pymine/database
+sudo dpkg-deb --build debian || exit 1
 
 DATESTAMP=`date "+%Y%m%d%H%M%S"`
 
-mv debian.deb /tmp/pymine_${PYMINE_VERSION}_${DATESTAMP}.deb
+mv debian.deb $USERDIR/pymine_${PYMINE_VERSION}_${DATESTAMP}.deb
+
+##################################################################
 
 exit 0
