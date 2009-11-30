ff9045370d6c930c9f7fde3d1c01479371406aa4
Alec.Muffett <Alec.Muffett@00126098-1528-11de-8b5c-13accb87f508>
2009-11-30 17:43:23 +0000

    crypto approaching seriousness.

--- mine-setup.sh
+++ mine-setup.sh
@@ -39,13 +39,13 @@ do
     read MINE_USER_EMAIL
 done
 
-PROMPT "Enter a SUPER USER login name for your mine (default: $DEFAULT_SUPERUSER)"
-read MINE_SUPERUSER
-test "x$MINE_SUPERUSER" = "x" && MINE_SUPERUSER=$DEFAULT_SUPERUSER
-
 PROMPT "Enter a ORDINARY USER login name for your mine (default: $DEFAULT_USER)"
 read MINE_USER
 test "x$MINE_USER" = "x" && MINE_USER=$DEFAULT_USER
+
+PROMPT "Enter a SUPER USER login name for your mine (default: $DEFAULT_SUPERUSER)"
+read MINE_SUPERUSER
+test "x$MINE_SUPERUSER" = "x" && MINE_SUPERUSER=$DEFAULT_SUPERUSER
 
 MINE_USER_DB=database/$MINE_USER/sqlite3.db
 
@@ -60,7 +60,7 @@ then
 fi
 
 # yes the args are visible during creation, no i can't be arsed yet to fix this
-MINE_SECRET=`./tools/keygen.py`
+MINE_SECRET=`python -c 'import os, util.base64_mine as b64 ; print b64.encode(os.urandom(66))'`
 
 if [ ! -f settings.py ]
 then
@@ -77,19 +77,28 @@ then
 fi
 
 # go for it
+echo ""
 make dbsync  || exit 1
 
-echo "Enter a password for SUPER USER '$MINE_SUPERUSER'"
-python manage.py createsuperuser --username $MINE_SUPERUSER --email $MINE_USER_EMAIL || exit 1
-
+echo ""
 while [ "x$MINE_USER_PASSWORD" = "x" ]
 do
     echo "Enter a password for ORDINARY USER '$MINE_USER'"
     PROMPT "Password:"
+    trap "stty echo ; exit" 1 2 3 15
+    stty -echo
     read MINE_USER_PASSWORD
+    stty echo
+    echo ""
+    trap "" 1 2 3 15
 
     PROMPT "Password (again):"
+    trap "stty echo ; exit" 1 2 3 15
+    stty -echo
     read MINE_USER_PASSWORD2
+    stty echo
+    echo ""
+    trap "" 1 2 3 15
 
     if [ "x$MINE_USER_PASSWORD" != "x$MINE_USER_PASSWORD2" ]
     then
@@ -99,8 +108,14 @@ do
 done
 DJANGO ./mineconfig.py create-user "$MINE_USER" "$MINE_USER_PASSWORD"
 
+echo ""
+echo "Enter a password for SUPER USER '$MINE_SUPERUSER'"
+python manage.py createsuperuser --username $MINE_SUPERUSER --email $MINE_USER_EMAIL || exit 1
+
+echo ""
 echo setting up crypto seeds and keys...
 DJANGO ./mineconfig.py init-crypto
 
+echo ""
 echo done.
 exit 0
