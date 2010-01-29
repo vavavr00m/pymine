2d596958995e1ccf469c6f69ada36454e4cbde66
Alec.Muffett <Alec.Muffett@00126098-1528-11de-8b5c-13accb87f508>
2010-01-29 13:16:27 +0000

    THIS COMMIT BREAKS PYMINE.  IF YOU NEED A STABLE PLATFORM USE 406 FOR THE NEXT FEW DAYS. safety commit.

--- trunk/api/__init__.py
+++ trunk/api/__init__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 ##
-## Copyright 2009 Adriana Lukas & Alec Muffett
+## Copyright 2009-2010 Adriana Lukas & Alec Muffett
 ##
 ## Licensed under the Apache License, Version 2.0 (the "License"); you
 ## may not use this file except in compliance with the License. You
