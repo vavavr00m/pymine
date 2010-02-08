ddac3a2060cce53e795aa60c3db3a6f57af47815
Alec.Muffett <Alec.Muffett@00126098-1528-11de-8b5c-13accb87f508>
2010-02-08 02:00:49 +0000

    model functional and administrable

---
+++ trunk/api/admin.py
@@ -0,0 +1,176 @@
+#!/usr/bin/python
+##
+## Copyright 2009-2010 Adriana Lukas & Alec Muffett
+##
+## Licensed under the Apache License, Version 2.0 (the "License"); you
+## may not use this file except in compliance with the License. You
+## may obtain a copy of the License at
+##
+## http://www.apache.org/licenses/LICENSE-2.0
+##
+## Unless required by applicable law or agreed to in writing, software
+## distributed under the License is distributed on an "AS IS" BASIS,
+## WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+## implied. See the License for the specific language governing
+## permissions and limitations under the License.
+##
+
+"""docstring goes here""" # :-)
+
+from models import Tag, Feed, Item, Comment, Vurl
+from models import TagXattr, FeedXattr, ItemXattr, CommentXattr, VurlXattr
+from models import Registry
+
+from django.contrib import admin
+
+##################################################################
+
+class RegistryAdmin(admin.ModelAdmin):
+    list_display = ('key', 'value', 'last_modified', 'created')
+    search_fields = ['key']
+    list_filter = ['created']
+    date_hierarchy = 'created'
+    fieldsets = [
+        (None, {'fields': ['key', 'value']}), 
+        ]
+
+admin.site.register(Registry, RegistryAdmin)
+
+##################################################################
+##################################################################
+##################################################################
+##################################################################
+##################################################################
+
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('name', 'last_modified', 'created')
+    search_fields = ['name', 'description']
+    list_filter = ['created']
+    date_hierarchy = 'created'
+    fieldsets = [
+        (None, {'fields': ['name']}), 
+        ('Advanced', {'fields': ['description', 'implies']}), 
+        ]
+
+admin.site.register(Tag, TagAdmin)
+
+##################################################################
+
+class FeedAdmin(admin.ModelAdmin):
+    list_display = ('name', 'version', 'last_modified', 'created')
+    search_fields = ['name', 'description']
+    list_filter = ['created']
+    date_hierarchy = 'created'
+    fieldsets = [
+        (None, {'fields': ['name', 'version', 'description']}), 
+        ('Tags', {'fields': ['tags', 'tags_require', 'tags_exclude']}), 
+        ('Advanced', {'fields': ['is_untrusted', 
+                                 'permitted_networks', 
+                                 'content_constraints', 
+                                 'embargo_before', 
+                                 'embargo_after']}), 
+        ]
+
+admin.site.register(Feed, FeedAdmin)
+
+##################################################################
+
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ('name', 'status', 'data_type', 'last_modified', 'created', 'hide_before', 'hide_after')
+    search_fields = ['name', 'description']
+    list_filter = ['created']
+    date_hierarchy = 'created'
+    fieldsets = [
+        (None, {'fields': ['name', 'status', 'data_type', 'description']}), 
+        ('Data', {'fields': ['data',  'data_remote_url', 'icon', 'icon_type', 'linked_items']}), 
+        ('Tags', {'fields': ['tags', 'item_for_feeds', 'item_not_feeds']}), 
+        ('Advanced', {'fields': ['hide_before', 'hide_after']}), 
+        ]
+
+admin.site.register(Item, ItemAdmin)
+
+##################################################################
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('title', 'from_feed',  'upon_item', 'last_modified', 'created')
+    search_fields = ['title', 'body']
+    list_filter = ['created']
+    date_hierarchy = 'created'
+    fieldsets = [
+        (None, {'fields': ['title', 'body']}), 
+        ('Advanced', {'fields': ['upon_item', 'from_feed']}), 
+        ]
+
+admin.site.register(Comment, CommentAdmin)
+
+##################################################################
+
+class VurlAdmin(admin.ModelAdmin):
+    list_display = ('name', 'link', 'use_temporary_redirect', 'last_modified', 'created')
+    search_fields = ['name']
+    list_filter = ['created']
+    date_hierarchy = 'created'
+    fieldsets = [
+        (None, {'fields': ['name', 'link', 'use_temporary_redirect']}), 
+        ('Advanced', {'fields': ['invalid_before', 'invalid_after']}), 
+        ]
+
+admin.site.register(Vurl, VurlAdmin)
+
+##################################################################
+##################################################################
+##################################################################
+##################################################################
+##################################################################
+
+class TagXattrAdmin(admin.ModelAdmin):
+    list_display = ('tag', 'key', 'value', 'last_modified', 'created')
+    search_fields = ['key', 'value']
+    list_filter = ['created']
+    date_hierarchy = 'created'
+
+admin.site.register(TagXattr, TagXattrAdmin)
+
+##################################################################
+
+class CommentXattrAdmin(admin.ModelAdmin):
+    list_display = ('comment', 'key', 'value', 'last_modified', 'created')
+    search_fields = ['key', 'value']
+    list_filter = ['created']
+    date_hierarchy = 'created'
+
+admin.site.register(CommentXattr, CommentXattrAdmin)
+
+##################################################################
+
+class ItemXattrAdmin(admin.ModelAdmin):
+    list_display = ('item', 'key', 'value', 'last_modified', 'created')
+    search_fields = ['key', 'value']
+    list_filter = ['created']
+    date_hierarchy = 'created'
+
+admin.site.register(ItemXattr, ItemXattrAdmin)
+
+##################################################################
+
+class FeedXattrAdmin(admin.ModelAdmin):
+    list_display = ('feed', 'key', 'value', 'last_modified', 'created')
+    search_fields = ['key', 'value']
+    list_filter = ['created']
+    date_hierarchy = 'created'
+
+admin.site.register(FeedXattr, FeedXattrAdmin)
+
+##################################################################
+
+class VurlXattrAdmin(admin.ModelAdmin):
+    list_display = ('vurl', 'key', 'value', 'last_modified', 'created')
+    search_fields = ['key', 'value']
+    list_filter = ['created']
+    date_hierarchy = 'created'
+
+admin.site.register(VurlXattr, VurlXattrAdmin)
+
+##################################################################
+##################################################################
+##################################################################
--- trunk/ui/views.py
+++ trunk/ui/views.py
@@ -16,3 +16,445 @@
 ##
 
 """docstring goes here""" # :-)
+
+from django.shortcuts import render_to_response
+
+##################################################################
+##################################################################
+##################################################################
+
+# this definition (create_comment) is auto-generated.
+# ensure that any changes are made via the generator.
+def create_comment(request, template, idz, **kwargs):
+    """
+    arguments: request, template, idz, **kwargs
+    implements: GET /ui/create/comment/(IDZ).html
+    returns: ...
+    """
+    s = {}
+    return render_to_response(template, s)
+
+##################################################################
+
+# this definition (dash_comments) is auto-generated.
+# ensure that any changes are made via the generator.
+def dash_comments(request, template, **kwargs):
+    """
+    arguments: request, template, **kwargs
+    implements: GET /ui/dash/comments.html
+    returns: ...
+    """
+    s = {}
+    return render_to_response(template, s)
+
+##################################################################
+
+# this definition (dash_feeds) is auto-generated.
+# ensure that any changes are made via the generator.
+def dash_feeds(request, template, **kwargs):
+    """
+    arguments: request, template, **kwargs
+    implements: GET /ui/dash/feeds.html
+    returns: ...
+    """
+    s = {}
+    return render_to_response(template, s)
+
+##################################################################
+
+# this definition (dash_items) is auto-generated.
+# ensure that any changes are made via the generator.
+def dash_items(request, template, **kwargs):
+    """
+    arguments: request, template, **kwargs
+    implements: GET /ui/dash/items.html
+    returns: ...
+    """
+    s = {}
+    return render_to_response(template, s)
+
+##################################################################
+
+# this definition (dash_tags) is auto-generated.
+# ensure that any changes are made via the generator.
+def dash_tags(request, template, **kwargs):
+    """
+    arguments: request, template, **kwargs
+    implements: GET /ui/dash/tags.html
+    returns: ...
+    """
+    s = {}
+    return render_to_response(template, s)
+
+##################################################################
+
+# this definition (dash_vurls) is auto-generated.
+# ensure that any changes are made via the generator.
+def dash_vurls(request, template, **kwargs):
+    """
+    arguments: request, template, **kwargs
+    implements: GET /ui/dash/vurls.html
+    returns: ...
+    """
+    s = {}
+    return render_to_response(template, s)
+
+##################################################################
+
+# this definition (delete_comment) is auto-generated.
+# ensure that any changes are made via the generator.
+def delete_comment(request, template, id, **kwargs):
+    """
+    arguments: request, template, id, **kwargs
+    implements: GET /ui/delete/comment/(ID).html
+    returns: ...
+    """
+    s = {}
+    return render_to_response(template, s)
+
+##################################################################
+
+# this definition (delete_feed) is auto-generated.
+# ensure that any changes are made via the generator.
+def delete_feed(request, template, id, **kwargs):
+    """
+    arguments: request, template, id, **kwargs
+    implements: GET /ui/delete/feed/(ID).html
+    returns: ...
+    """
+    s = {}
+    return render_to_response(template, s)
+
+##################################################################
+
+# this definition (delete_item) is auto-generated.
+# ensure that any changes are made via the generator.
+def delete_item(request, template, id, **kwargs):
+    """
+    arguments: request, template, id, **kwargs
+    implements: GET /ui/delete/item/(ID).html
+    returns: ...
+    """
+    s = {}
+    return render_to_response(template, s)
+
+##################################################################
+
+# this definition (delete_tag) is auto-generated.
+# ensure that any changes are made via the generator.
+def delete_tag(request, template, id, **kwargs):
+    """
+    arguments: request, template, id, **kwargs
+    implements: GET /ui/delete/tag/(ID).html
+    returns: ...
+    """
+    s = {}
+    return render_to_response(template, s)
+
+##################################################################
+
+# this definition (delete_vurl) is auto-generated.
+# ensure that any changes are made via the generator.
+def delete_vurl(request, template, id, **kwargs):
+    """
+    arguments: request, template, id, **kwargs
+    implements: GET /ui/delete/vurl/(ID).html
+    returns: ...
+    """
+    s = {}
+    return render_to_response(template, s)
+
+##################################################################
+
+# this definition (list_comments) is auto-generated.
+# ensure that any changes are made via the generator.
+def list_comments(request, template, idz, **kwargs):
+    """
+    arguments: request, template, idz, **kwargs
+    implements: GET /ui/list/comments/(IDZ).html
+    returns: ...
+    """
+    s = {}
+    return render_to_response(template, s)
+
+##################################################################
+
+# this definition (list_feeds) is auto-generated.
+# ensure that any changes are made via the generator.
+def list_feeds(request, template, **kwargs):
+    """
+    arguments: request, template, **kwargs
+    implements: GET /ui/list/feeds.html
+    returns: ...
+    """
+    s = {}
+    return render_to_response(template, s)
+
+##################################################################
+
+# this definition (list_items) is auto-generated.
+# ensure that any changes are made via the generator.
+def list_items(request, template, **kwargs):
+    """
+    arguments: request, template, **kwargs
+    implements: GET /ui/list/items.html
+    returns: ...
+    """
+    s = {}
+    return render_to_response(template, s)
+
+##################################################################
+
+# this definition (list_tags) is auto-generated.
+# ensure that any changes are made via the generator.
+def list_tags(request, template, **kwargs):
+    """
+    arguments: request, template, **kwargs
+    implements: GET /ui/list/tags.html
+    returns: ...
+    """
+    s = {}
+    return render_to_response(template, s)
+
+##################################################################
+
+# this definition (list_vurls) is auto-generated.
+# ensure that any changes are made via the generator.
+def list_vurls(request, template, **kwargs):
+    """
+    arguments: request, template, **kwargs
+    implements: GET /ui/list/vurls.html
+    returns: ...
+    """
+    s = {}
+    return render_to_response(template, s)
+
+##################################################################
+
+# this definition (read_comment) is auto-generated.
+# ensure that any changes are made via the generator.
+def read_comment(request, template, id, **kwargs):
+    """
+    arguments: request, template, id, **kwargs
+    implements: GET /ui/read/comment/(ID).html
+    returns: ...
+    """
+    s = {}
+    return render_to_response(template, s)
+
+##################################################################
+
+# this definition (read_feed) is auto-generated.
+# ensure that any changes are made via the generator.
+def read_feed(request, template, id, **kwargs):
+    """
+    arguments: request, template, id, **kwargs
+    implements: GET /ui/read/feed/(ID).html
+    returns: ...
+    """
+    s = {}
+    return render_to_response(template, s)
+
+##################################################################
+
+# this definition (read_item) is auto-generated.
+# ensure that any changes are made via the generator.
+def read_item(request, template, id, **kwargs):
+    """
+    arguments: request, template, id, **kwargs
+    implements: GET /ui/read/item/(ID).html
+    returns: ...
+    """
+    s = {}
+    return render_to_response(template, s)
+
+##################################################################
+
+# this definition (read_tag) is auto-generated.
+# ensure that any changes are made via the generator.
+def read_tag(request, template, id, **kwargs):
+    """
+    arguments: request, template, id, **kwargs
+    implements: GET /ui/read/tag/(ID).html
+    returns: ...
+    """
+    s = {}
+    return render_to_response(template, s)
+
+##################################################################
+
+# this definition (read_vurl) is auto-generated.
+# ensure that any changes are made via the generator.
+def read_vurl(request, template, id, **kwargs):
+    """
+    arguments: request, template, id, **kwargs
+    implements: GET /ui/read/vurl/(ID).html
+    returns: ...
+    """
+    s = {}
+    return render_to_response(template, s)
+
+##################################################################
+
+# this definition (render) is auto-generated.
+# ensure that any changes are made via the generator.
+def render(request, template, **kwargs):
+    """
+    arguments: request, template, **kwargs
+    implements: GET /ui/create/feed.html
+    implements: GET /ui/create/item.html
+    implements: GET /ui/create/tag.html
+    implements: GET /ui/create/vurl.html
+    implements: GET /ui/dash/search.html
+    implements: GET /ui/dash/settings.html
+    returns: ...
+    """
+    s = {}
+    return render_to_response(template, s)
+
+##################################################################
+
+# this definition (search_comments) is auto-generated.
+# ensure that any changes are made via the generator.
+def search_comments(request, template, **kwargs):
+    """
+    arguments: request, template, **kwargs
+    implements: GET /ui/search/comments.html
+    returns: ...
+    """
+    s = {}
+    return render_to_response(template, s)
+
+##################################################################
+
+# this definition (search_feeds) is auto-generated.
+# ensure that any changes are made via the generator.
+def search_feeds(request, template, **kwargs):
+    """
+    arguments: request, template, **kwargs
+    implements: GET /ui/search/feeds.html
+    returns: ...
+    """
+    s = {}
+    return render_to_response(template, s)
+
+##################################################################
+
+# this definition (search_items) is auto-generated.
+# ensure that any changes are made via the generator.
+def search_items(request, template, **kwargs):
+    """
+    arguments: request, template, **kwargs
+    implements: GET /ui/search/items.html
+    returns: ...
+    """
+    s = {}
+    return render_to_response(template, s)
+
+##################################################################
+
+# this definition (search_tags) is auto-generated.
+# ensure that any changes are made via the generator.
+def search_tags(request, template, **kwargs):
+    """
+    arguments: request, template, **kwargs
+    implements: GET /ui/search/tags.html
+    returns: ...
+    """
+    s = {}
+    return render_to_response(template, s)
+
+##################################################################
+
+# this definition (search_vurls) is auto-generated.
+# ensure that any changes are made via the generator.
+def search_vurls(request, template, **kwargs):
+    """
+    arguments: request, template, **kwargs
+    implements: GET /ui/search/vurls.html
+    returns: ...
+    """
+    s = {}
+    return render_to_response(template, s)
+
+##################################################################
+
+# this definition (update_comment) is auto-generated.
+# ensure that any changes are made via the generator.
+def update_comment(request, template, id, **kwargs):
+    """
+    arguments: request, template, id, **kwargs
+    implements: GET /ui/update/comment/(ID).html
+    returns: ...
+    """
+    s = {}
+    return render_to_response(template, s)
+
+##################################################################
+
+# this definition (update_feed) is auto-generated.
+# ensure that any changes are made via the generator.
+def update_feed(request, template, id, **kwargs):
+    """
+    arguments: request, template, id, **kwargs
+    implements: GET /ui/update/feed/(ID).html
+    returns: ...
+    """
+    s = {}
+    return render_to_response(template, s)
+
+##################################################################
+
+# this definition (update_item) is auto-generated.
+# ensure that any changes are made via the generator.
+def update_item(request, template, id, **kwargs):
+    """
+    arguments: request, template, id, **kwargs
+    implements: GET /ui/update/item/(ID).html
+    returns: ...
+    """
+    s = {}
+    return render_to_response(template, s)
+
+##################################################################
+
+# this definition (update_tag) is auto-generated.
+# ensure that any changes are made via the generator.
+def update_tag(request, template, id, **kwargs):
+    """
+    arguments: request, template, id, **kwargs
+    implements: GET /ui/update/tag/(ID).html
+    returns: ...
+    """
+    s = {}
+    return render_to_response(template, s)
+
+##################################################################
+
+# this definition (update_vurl) is auto-generated.
+# ensure that any changes are made via the generator.
+def update_vurl(request, template, id, **kwargs):
+    """
+    arguments: request, template, id, **kwargs
+    implements: GET /ui/update/vurl/(ID).html
+    returns: ...
+    """
+    s = {}
+    return render_to_response(template, s)
+
+##################################################################
+
+# this definition (version) is auto-generated.
+# ensure that any changes are made via the generator.
+def version(request, template, **kwargs):
+    """
+    arguments: request, template, **kwargs
+    implements: GET /ui/version.html
+    returns: ...
+    """
+    s = {}
+    return render_to_response(template, s)
+
+##################################################################
+##################################################################
+##################################################################
